5a2e38b587e7f43cca3f8a3da71c62b4dd5293c3
Danny Sandoval <dannygamer@live.com>
2019-03-03 09:29:24 -0800

    Update Rider.java

--- Rider.java
+++ Rider.java
@@ -8,9 +8,9 @@ public class Rider {
     int time_leave;
     int time_return;
     String day;
-    int driver_distance_preference;
+    double driver_distance_preference;
     boolean is_driver;
-    
+
     Rider() {
         name = "";
         address = "";
@@ -43,11 +43,11 @@ public class Rider {
         this.time_leave_minute = time_leave_minute;
         this.time_leave = time_leave_hour * 60 + time_leave_minute;
     }
-    
+
     public void set_day(String day) {
         this.day = day;
     }
-    public void set_driver_distance_preference(int set_driver_distance_preference) {
+    public void set_driver_distance_preference(double driver_distance_preference) {
         this.driver_distance_preference = driver_distance_preference;
     }
     public void set_is_driver(boolean is_driver){
@@ -57,13 +57,8 @@ public class Rider {
     public String get_name() {
         return name;
     }
-    public int get_time_leave(){
-      return time_leave;
-    }
-    public int get_time_return(){
-      return time_return;
-    }
-    public int get_driver_distance_preference(){
+
+    public double get_driver_distance_preference(){
         return driver_distance_preference;
     }
     public int get_time_return_hour(){
@@ -78,6 +73,12 @@ public class Rider {
     public int get_time_leave_minute(){
         return time_leave_minute;
     }
+    public int get_time_leave(){
+        return time_leave;
+    }
+    public int get_time_return(){
+        return time_return;
+    }
     public String get_day(){
         return day;
     }
@@ -89,9 +90,9 @@ public class Rider {
     }
     public void print() {
         System.out.println("Name: " + name);
-        System.out.println("Location Start: " + address);
+        System.out.println("Address: " + address);
         System.out.println("Time of Departure: " + time_leave);
-        System.out.println("Expected time of arrival: " + time_arrive);
+        System.out.println("Time of Return: " + time_return);
         System.out.println("Day: " + day);
         if (is_driver)
             System.out.println("Distance Preference: " + driver_distance_preference);
