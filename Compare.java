bec870d1ba2baa2b20bb79071d20c490358875f9
Damon Nutter <36419082+NutterDamon@users.noreply.github.com>
2019-03-03 00:24:35 -0800

    Create Compare.java

---
+++ Compare.java
@@ -0,0 +1,41 @@
+public class Compare {
+  ArrayList<Rider> riders = new Arraylist();
+  Rider rider_to_compare;
+
+  Compare(Rider rider_to_compare, ArrayList<Rider> riders){
+    this.rider_to_compare = rider_to_compare;
+    this.riders = riders;
+  }
+  public String find_match(){
+    String return_value;
+    //check if rider distance is compatible
+
+    //check if rider times are compatible
+    int compare_time;
+    for (int i = 0; i < riders.size(); i++){
+      //Is someone a driver?
+      if (rider_to_compare.get_is_driver() || riders.get(i).get_is_driver()){
+        //Is Rider to compare <= array value?
+        if (rider_to_compare.get_time_leave() <= riders[i].get_time_leave()){
+          compare_time = riders.get(i).get_time_leave() - rider_to_compare.get_time_leave();
+          
+        } else {
+          compare_time = rider_to_compare.get_time_leave() - ridersget(i).get_time_leave();
+        }
+        // Is time less than 30 minutes?
+        if (compare_time <= 30){
+          return_value = riderget(i).get_name;
+        } else {
+          return_value = "NULL";
+        }
+        break;
+      } else {
+        return_value = "NULL";
+      }
+    }
+    return return_value;
+  }
+  public int compare_address(Rider r1, Rider r2){
+    
+  }
+}
