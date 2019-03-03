bfa6ef38f6d3fd09156ff45eed698a70ef4bda74
Damon Nutter <36419082+NutterDamon@users.noreply.github.com>
2019-03-02 21:49:02 -0800

    Update Rider.java

--- Rider.java
+++ Rider.java
@@ -3,25 +3,36 @@ public class Rider
     String location_start;
     String location_end;
     String time_leave;
+    int time_return;
     String time_arrive;
     String name;
     String day;
-
+    bool no_smoke;
+    int driver_distance_preference;
+    bool is_driver;
+    
     Rider()
     {
-        name = "Damon";
+        name = "";
         location_start = "";
         location_end = "";
         time_leave = "";
         time_arrive = "";
+        time_return = 0;
         day = "";
+        no_smoke = false;
+        driver_distance_preference = 0;
+        bool is_driver = false;
+        
     }
-
+    // Set functions -----------------------------------------------------------------------------
     public void set_name(String name)
     {
         this.name = name;
     }
-
+    public void set_time_return(String time_return){
+        this.time_return = time_return;
+    }
     public void set_location_start(String location_start)
     {
         this.location_start = location_start;
@@ -46,12 +57,29 @@ public class Rider
     {
         this.day = day;
     }
-
+    public void set_no_smoking (bool no_smoke) {
+        this.no_smoke = no_smoke;
+    }
+    public void set_driver_distance_preference(int set_driver_distance_preference) {
+        this.driver_distance_preference = driver_distance_preference;
+    }
+    //Get functions-------------------------------------------------------
     public String get_name()
     {
         return name;
     }
-
+    public int get_driver_distance_preference(){
+        return driver_distance_preference;
+    }
+    public int get_time_return(){
+        return time_return;
+    }
+    public int get_time_leave(){
+        return time_leave;
+    }
+    public String get_day(){
+        return day;
+    }
     public void print()
     {
         System.out.println("Name: " + name);
